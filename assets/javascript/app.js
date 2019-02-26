4e06f8576bddad03f18c02c2969e47c57d713268
Fred Astaire <lancebd@thegodardgroup.com>
2019-02-26 15:28:21 -0500

    Timers mostly in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,58 +8,50 @@
 // Variables needed
 
 var questionBank = [
-  { theQuestion: "How many Oscars did Titanic win?", answerA: "Four", answerB: "Nine", answerC: "Eleven", answerD: "Thirteen", theBlurb: "In 1998, Titanic received 11 Academy Awards, including Best Picture, Best Director, Best Visual Effects, and Best Actress for Kate Winslet.", rightAns: "C" },
+  { theQuestion: "How many Academy Awards did Titanic win?", answerA: "Four", answerB: "Nine", answerC: "Eleven", answerD: "Thirteen", theBlurb: "In 1998, Titanic received 11 Academy Awards, including Best Picture, Best Director, Best Visual Effects, and Best Actress for Kate Winslet.", rightAns: "C" },
   { theQuestion: "Which of the following films did NOT win the Best Foreign Language Film Academy Award?", answerA: "Life is Beautiful (La vita è bella)", answerB: "Crouching Tiger, Hidden Dragon (Wo hu cang long)", answerC: "Babette’s Feast (Babettes gæstebud)", answerD: "Amélie (Le fabuleux destin d'Amélie Poulain)", theBlurb: "Amélie was nominated for five Oscars – Foreign Language Film, Best Original Screenplay, Best Cinematography, Best Sound Mixing, and Best Production Design – but did not win in any category.", rightAns: "D" },
   { theQuestion: "Who has won the most Academy Awards for acting?", answerA: "Katharine Hepburn", answerB: "Walter Brennan", answerC: "Jack Nicholson", answerD: "Ingrid Bergman", theBlurb: "Katharine Hepburn has won 4 acting awards: Best Actress- Morning Glory (1933), Guess Who's Coming to Dinner? (1967), The Lion in Winter (1968), On Golden Pond (1981). Jack Nicholson, Ingrid Bergman, and Walter Brennan all have won 3 acting Academy Awards.", rightAns: "A" },
   { theQuestion: "Who has won the most Oscars for directing?", answerA: "Steven Spielberg", answerB: "John Ford", answerC: "William Wyler", answerD: "Frank Capra", theBlurb: "John Ford has won 4 Best Director Oscars for The Informer (1935), The Grapes of Wrath (1940), How Green Was My Valley (1941), and The Quiet Man (1952). He was also nominated for Stagecoach (1939). William Wyler and Frank Capra both have won 3 times.", rightAns: "B" },
   { theQuestion: "Which actor has been nominated most often for Oscars?", answerA: "Spencer Tracy", answerB: "Laurence Olivier", answerC: "Al Pacino", answerD: "Jack Nicholson", theBlurb: "Jack Nicholson has been nominated 12 times for his performances.", rightAns: "D" },
   { theQuestion: "Which film was the first to win Academy Awards for Best Picture, Best Director, Best Actor, Best Actress, and Best Screenplay?", answerA: "One Flew Over the Cuckoo's", answerB: "Gone With the Wind", answerC: "It Happened One Night", answerD: "The Silence of the Lambs", theBlurb: "Frank Capra's influential romantic comedy It Happened One Night became the first film to perform a clean sweep of the top five categories; Best Picture, Best Director, Best Actor, Best Actress and Best Screenplay. This feat would later be duplicated by One Flew Over the Cuckoo's Nest in 1976 and The Silence of the Lambs in 1992.", rightAns: "C" },
-  { theQuestion: "Who is the only African American woman to win a Best Actress Oscar?", answerA: "Hattie McDaniel", answerB: "Halle Berry", answerC: "Whoopi Goldberg", answerD: "Jennifer Hudson", theBlurb: "Halle Berry in Monster's Ball (2001)", rightAns: "B" },
+  { theQuestion: "Who is the only African American woman to win a Best Actress Oscar?", answerA: "Hattie McDaniel", answerB: "Halle Berry", answerC: "Whoopi Goldberg", answerD: "Jennifer Hudson", theBlurb: "Shockingly, Halle Berry is the only African American to win the Best Actress award, for her performance in Monster's Ball (2001).", rightAns: "B" },
   { theQuestion: "Which film series has won more than one Academy Award for Best Picture?", answerA: "The Lord of the Rings", answerB: "Rocky", answerC: "The Godfather", answerD: "James Bond", theBlurb: "The Godfather series is the only film series to win more than one Best Picture award, for The Godfather (1972) and The Godfather: Part II (1974).", rightAns: "C" },
-  { theQuestion: "Who is the oldest woman to win an Academy Award for acting?", answerA: "Katharine Hepburn", answerB: "Helen Mirren", answerC: "Jessica Tandy", answerD: "Geraldine Page", theBlurb: "Oldest winner of an acting award: Jessica Tandy, age 80 (Best Actress, Driving Miss Daisy, 1989)", rightAns: "C" },
-  { theQuestion: "Who has won the most Oscars?", answerA: "Katharine Hepburn", answerB: "Jack Nicholson", answerC: "Meryl Streep", answerD: "Walt Disney", theBlurb: "Walt Disney won the most Academy Awards, with 22 competitive and 4 honorary.", rightAns: "D" }
+  { theQuestion: "Who is the oldest woman to win an Academy Award for acting?", answerA: "Katharine Hepburn", answerB: "Helen Mirren", answerC: "Jessica Tandy", answerD: "Geraldine Page", theBlurb: "Jessica Tandy was the oldest winner of an acting award when she won the Best Actress Oscar for Driving Miss Daisy (1989).", rightAns: "C" },
+  { theQuestion: "Who has won the most Oscars?", answerA: "Katharine Hepburn", answerB: "Jack Nicholson", answerC: "Meryl Streep", answerD: "Walt Disney", theBlurb: "Walt Disney won the most Academy Awards, with 22 competitive and 4 honorary Oscars.", rightAns: "D" }
 ]
 var right = 0; // number of right answers
 var wrong = 0; // number of wrong answers
 var rightAns = ""; // placeholder for the right answer for each question
 var blurbText = ""; // placeholder for explanation of correctAnswer
 var currentQuestion = 0; // variable to iterate through the array
-var count = "";
-var interval = '';
+var count;
+var intervalID;
+// var intervalID = setInterval(countDown, 1000); // place variable at global level so it can be used throughout
 
 $(document).ready(function () {
   displayQuestion(questionBank[currentQuestion])
-  questTimer();
-  // $("#statusBar").text("You have " + count + "seconds to answer this question");
   $(".answerBtn").click(function () {
     var playerGuess = $(this).data("letter");
-    stop();
+    $("#statusBar").text("");
     evaluateGuess(playerGuess);
   });
 
   function questTimer() {
-    var count = 30;
-    var interval = setInterval(countDown, 1000);
-    function countDown() {
-      $("#statusBar").text("You have " + count + " seconds left...");
-      count--;
-      if (count === 0) {
-        stop();
-        timeUp();
-        wrong = wrong++;
-      }
-    }
+    count = 30;
+    clearInterval(intervalID);
+    intervalID = setInterval(countDown, 1000);
+    countDown();
   }
 
   function evaluateGuess(guess) {
-    stop();
+    $("#statusBar").text("");
     $("#blurbBar").text(questionBank[currentQuestion - 1].theBlurb);
     if (guess === rightAns) {
-      $("#askedQuestion").html("That's right!");
+      $("#statusBar").html("That's right!");
       right = right + 1;
       console.log(right + " right answers");
     } else {
-      $("#askedQuestion").html("Nope, that's not it. ");
+      $("#statusBar").html("Nope, that's not it. ");
       wrong = wrong + 1;
       console.log(wrong + " wrong answers");
     }
@@ -70,12 +62,9 @@ $(document).ready(function () {
     if (currentQuestion == 10) {
       setTimeout(totalScore, 5000);
     } else {
+      // questTimer();
       displayQuestion(questionBank[currentQuestion]);
     }
-  }
-
-  function timeUp() {
-    $("#statusBar").text("Time's up!");
   }
 
   function totalScore() {
@@ -92,7 +81,7 @@ $(document).ready(function () {
   }
 
   function displayQuestion(foo) {
-    setTimeout(questTimer, 30000);
+    questTimer();
     $("#askedQuestion").text(foo.theQuestion);
     $("#firstAnswer").text(foo.answerA);
     $("#secondAnswer").text(foo.answerB);
@@ -105,15 +94,29 @@ $(document).ready(function () {
   }
 
   function stop() {
-    clearInterval(interval);
+    clearInterval(intervalID);
     $("#statusBar").text("");
   }
 
+  function countDown() {
+    $("#statusBar").text("You have " + count + " seconds left...");
+    count--;
+    if (count === 0) {
+      clearInterval(intervalID);
+      $("#statusBar").text("");
+      timeUp();
+    }
+  }
+  
+  function timeUp() {
+    $("#statusBar").text("Time's up!");
+    $("#blurbBar").text(questionBank[currentQuestion - 1].theBlurb);
+    setTimeout(loadNewQuestion, 5000);
+    wrong = wrong++;
+  }
 });
 
   // DOM elements
-  // game name
-  // design elements 
   // pictures to illustrate each question
   // Six writable fields: 
   // 1. Messages to player ("ready?" "Correct!" "Nope, that's not it" etc)
@@ -123,18 +126,4 @@ $(document).ready(function () {
   // 5. Answer 3 - needs to capture user click 
   // 6. Answer 4 - needs to capture user click 
   //     Repurpose some of those fields to write results at the end
-  // countdown clock
   // hover, etc. pseudostyles for answers
-
-
-  // Design Notes
-  // use bootstrap -  mobile first
-  // 
-
-
-
-  // function delayedReset() {
-  //   setTimeout(resetGame, 3000);
-  // }
-
-
